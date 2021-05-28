827bbb02a031a5641e6629f14ec7876dd99752ea
Vinicius Costa Santos <viniciusath@hotmail.com>
2021-05-28 12:52:22 -0300

    nada demais

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -23,8 +23,8 @@
 <nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg" color-on-scroll="100">
     <div class="container">
         <div class="navbar-translate">
-            <a class="navbar-brand" href="#">
-                {{  str_replace('-', ' ', config('app.name', 'Laravel'))  }} </a>
+            <a class="navbar-brand" href="#"><b>
+                {{  str_replace('-', ' ', config('app.name', 'Laravel'))  }} </b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="navbar-toggler-icon"></span>
